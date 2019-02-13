f6fc0ef184834a6a184766d40cb0f896b23ada3b
theryanclausen <theryanclausen@gmail.com>
2019-02-13 11:30:11 -0700

    item room def

--- src/room.py
+++ src/room.py
@@ -1,2 +1,21 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+from item import Item 
+
+
+
+class Room(Item):
+    def __init__(self, name, description, list):
+        self.list = list
+        super().__init__(name, description)
+    def enter(self):
+        print(f'You enter the {self.name}. {self.description}') 
+    def unpack(self):
+        return ', '.join(self.list)
+    def check(self):
+        print(f'Searching the {self.name}, you find {self.unpack()}') 
+        
+
+room = Room('house', 'spooky', ['zip ties', 'butterflies'])
+
+room.check()
